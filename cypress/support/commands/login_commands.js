ccaa16a19b873b8c38faf5f232c819db8e696576
Edimilson Candido <edimilson.candido@grupoquality.local>
2024-08-29 09:02:07 -0300

    estrutura login

--- cypress/support/commands/login_commands.js
+++ cypress/support/commands/login_commands.js
@@ -1,9 +1,7 @@
 const { ACESS_PAGE } = require('../page_elements/acess_page')
 
 
-Cypress.Commands.add('validarBotaoLogin', (text) => {
+Cypress.Commands.add('validarBotaoLogin', (text, text2) => {
     cy.get(ACESS_PAGE.LOGIN.BTN_LOGIN).should('be.visible').should('contain.text', text).click()
+    cy.get(ACESS_PAGE.LOGIN.HEADER_MODAL).should('be.visible').should('contain.txt',text2)
 })
-
-
-
