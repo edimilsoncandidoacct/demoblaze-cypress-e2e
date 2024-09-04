16aece4df158d9dd05877dfa4250ef9aca9982ec
Edimilson Candido <edimilson.candido@grupoquality.local>
2024-09-04 08:58:56 -0300

    login sucesso ok

--- cypress/support/commands/login_commands.js
+++ cypress/support/commands/login_commands.js
@@ -8,11 +8,14 @@ Cypress.Commands.add('validarBotaoLogin', (text) => {
 Cypress.Commands.add('validarHeaderModalLogin', (text) => {
     cy.get(ACESS_PAGE.LOGIN.HEADER_MODAL).should('be.visible').should('contain.text', text)
 })
-Cypress.Commands.add('validarLoginSucess', (user, text, pass) => {
-    cy.get(ACESS_PAGE.LOGIN.INPUT_USERNAME).should('be.visible').type(user)
-    // cy.get(ACESS_PAGE.LOGIN.INPUT_PASSWORD).should('be.visible').type(pass)
-    // cy.get(ACESS_PAGE.LOGIN.BTN_MODAL_LOGIN).should('be.visible').click()
-    // cy.get(HEADER_PAGE.LOGIN.MSG_SUCESS).should('be.visible').should('contain.text', text)
+Cypress.Commands.add('validarLoginSucess', (user, pass, text) => {
+    cy.get(ACESS_PAGE.LOGIN.INPUT_USERNAME).should('be.visible')
+        .click() // Clica no campo para garantir o foco
+        .clear() // Limpa o campo
+        .type(user, { delay: 100 })
+     cy.get(ACESS_PAGE.LOGIN.INPUT_PASSWORD).should('be.visible').type(pass)
+     cy.get(ACESS_PAGE.LOGIN.BTN_MODAL_LOGIN).should('be.visible').click()
+     cy.get(HEADER_PAGE.LOGIN.MSG_SUCESS).should('be.visible').should('contain.text', text)
 })
 
 
