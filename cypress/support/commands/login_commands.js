fead705e4a748260ba7cec5ef19fe82047d12ee3
Gerson Dias <gersoncdias1@gmail.com>
2024-09-13 08:54:53 -0300

    refatoração estrutural

--- cypress/support/commands/login_commands.js
+++ cypress/support/commands/login_commands.js
@@ -18,7 +18,7 @@ Cypress.Commands.add('validarLogin', (user, pass) => {
     .click() // Clica no campo para garantir o foco
     .clear() // Limpa o campo
     .type(user, { delay: 100 });
-  cy.get(ACESS_PAGE.LOGIN.INPUT_PASSWORD).should('be.visible').type(pass);
+  cy.get(ACESS_PAGE.LOGIN.INPUT_PASSWORD).should('be.visible').type(pass,{ log: false });
   cy.get(ACESS_PAGE.LOGIN.BTN_MODAL_LOGIN).should('be.visible').click();
 });
 
@@ -28,20 +28,28 @@ Cypress.Commands.add('validarMensagem', (text) => {
     .should('contain.text', text);
 });
 
-Cypress.Commands.add('validarUsernameIncorreto', (text) => {
-  // Intercepta a requisição POST para a API de login
-  cy.intercept('POST', '**/login').as('loginRequest');
+// Esta função não tem um bom uso quando é executado todos os testes, pois sua requeste para api de login é chamada em mais de uma situação
+// Cypress.Commands.add('validarUsernameIncorreto', (text) => {
+//   // Intercepta a requisição POST para a API de login
+//   cy.intercept('POST', '**/login').as('loginRequest');
 
-  // Espera a requisição e verifica o status da resposta e a mensagem de erro
-  cy.wait('@loginRequest').then((interception) => {
-    // Verifica se a resposta da API foi OK (200)
-    expect(interception.response.statusCode).to.equal(200);
+//   // Espera a requisição e verifica o status da resposta e a mensagem de erro
+//   cy.wait('@loginRequest').then((interception) => {
+//     // Verifica se a resposta da API foi OK (200)
+//     expect(interception.response.statusCode).to.equal(200);
 
-    // Verifica se a resposta contém uma mensagem de erro
-    if (interception.response.body.errorMessage) {
-      // Se houver uma mensagem de erro, falha o teste
-      expect(interception.response.body.errorMessage).to.not.be.empty;
-      expect(interception.response.body.errorMessage).to.equal(text);
-    }
+//     // Verifica se a resposta contém uma mensagem de erro
+//     if (interception.response.body.errorMessage) {
+//       // Se houver uma mensagem de erro, falha o teste
+//       expect(interception.response.body.errorMessage).to.not.be.empty;
+//       expect(interception.response.body.errorMessage).to.equal(text);
+//     }
+//   });
+// });
+
+Cypress.Commands.add('validarAlert', (text) => {
+  cy.on('window:alert', (txt) => {
+    expect(txt).to.contains(text);
   });
 });
+
