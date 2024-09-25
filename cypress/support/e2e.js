d5b0afa25955283c1aecf36ec877f6f988d732f7
Edimilson Candido <edimilson.candido@grupoquality.local>
2024-09-25 09:14:41 -0300

    plugin api

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -2,6 +2,7 @@ import './commands/ui_commands/home_commands';
 import './commands/ui_commands/signup_commands';
 import './commands/ui_commands/login_commands';
 import './commands/api_commands/api_login_commands';
+import 'cypress-plugin-api';
 Cypress.on('uncaught:exception', (err, runnable) => {
   return false;
 });
