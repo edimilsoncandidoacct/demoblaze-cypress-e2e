94858f82475e52d4bc82697c30bd6fdab9f9c66d
Edimilson Candido <edimilson.candido@grupoquality.local>
2024-09-27 09:02:08 -0300

    command api

--- cypress/support/commands/api_commands/api_login_commands.js
+++ cypress/support/commands/api_commands/api_login_commands.js
@@ -1,5 +1,17 @@
 // cypress/support/commands.js
 Cypress.Commands.add('apiLogin', (username, password) => {
+  cy.request({
+    method: 'POST',
+    url: Cypress.env('apiLoginUrl'), // Acessa a URL do arquivo de config
+    headers: {},
+    body: {
+      username: username,
+      password: password
+    }
+  });
+});
+
+Cypress.Commands.add('apiLoginPlugin', (username, password) => {
   cy.api({
     method: 'POST',
     url: Cypress.env('apiLoginUrl'), // Acessa a URL do arquivo de config
