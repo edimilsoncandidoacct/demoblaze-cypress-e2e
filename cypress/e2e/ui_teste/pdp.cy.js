0ecf40a9138d0c3c325984cb8e628752aa4f055a
Edimilson Candido <edimilson.candido@grupoquality.local>
2024-10-10 09:12:12 -0300

    customizacao pdp

--- cypress/e2e/ui_teste/pdp.cy.js
+++ cypress/e2e/ui_teste/pdp.cy.js
@@ -1,20 +1,15 @@
-const validation_data_home = require('../../fixtures/home.json');
 const validation_data_pdp = require('../../fixtures/pdp.json');
 
-describe('template spec', () => {
+describe('teste de pdp', () => {
   beforeEach(() => {
-    cy.visit('/');
-    cy.validateProductTitle(validation_data_home.product_name.nokia);
-    cy.validateProductLink(validation_data_home.product_name.nokia);
+    cy.visit(
+      Cypress.config().baseUrl +
+        validation_data_pdp.pdp_produtos.link_pdp +
+        validation_data_pdp.pdp_produtos.id_produto
+    );
   });
-    it('Deve validar a imagem, título e descrição do segundo produto', () => {
+  it('Deve validar a imagem, título e descrição do segundo produto', () => {
     cy.validarImagemProduto();
     cy.validarTituloNome(validation_data_pdp.pdp_descricoes.titulo_name);
   });
-
 });
-
-
-
-
-
