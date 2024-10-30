fa7980bf1c21d01e7af5272a359513f07dfd1c8a
Edimilson Candido <edimilson.candido@grupoquality.local>
2024-10-30 09:03:20 -0300

    coloca o produto no cart

--- cypress/e2e/ui_teste/cart.cy.js
+++ cypress/e2e/ui_teste/cart.cy.js
@@ -4,6 +4,7 @@ const env_data_user = require('../../../cypress.env.json');
 const validation_data_pdp = require('../../fixtures/pdp.json');
 
 describe('teste de cart', () => {
+  let token;
   beforeEach(() => {
     cy.apiLogin(
       Cypress.env('apiUrl') + validation_data_parameter.login_parameter,
@@ -11,7 +12,7 @@ describe('teste de cart', () => {
       env_data_user.user.password_hash_api
     ).then((response) => {
       // Extrai e define o cookie com o token
-      const token = response.body.replace('Auth_token: ', '');
+      token = response.body.replace('Auth_token: ', '');
       cy.setCookie('tokenp_', token); // Define o cookie com o nome 'tokenp_' e o valor do token
     });
     cy.visit(
@@ -21,12 +22,11 @@ describe('teste de cart', () => {
   it('validar pagina do cart', () => {
     cy.addToCart(
       Cypress.env('apiUrl') + validation_data_parameter.addtocart_parameter,
-      {
-        id: "c172115a-0a79-2ec5-b90b-5a928dc4ccff",
-        cookie: "ZWRpY2FuZGlkbzE3MzAxOTU=",
-        prod_id: validation_data_pdp.pdp_produtos.id_produto
-      }
+
+      'c172115a-0a79-2ec5-b90b-5a928dc4ccff',
+      token,
+      validation_data_pdp.pdp_produtos.id_produto,
+      true
     );
   });
-  
 });
